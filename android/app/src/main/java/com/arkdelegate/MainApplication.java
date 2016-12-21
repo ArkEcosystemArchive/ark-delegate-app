6476195eeb5b1decd6c97e826477c063a1d4d853
Guillaume Verbal <guillaume.verbal@gmail.com>
2016-12-21 23:30:52 +0800

    add shoutem ui

--- android/app/src/main/java/com/arkdelegate/MainApplication.java
+++ android/app/src/main/java/com/arkdelegate/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNSharePackage(),
+            new VectorIconsPackage()
       );
     }
   };
--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react'
 import { View, ScrollView, Text } from 'react-native'
+import { Heading, NavigationBar, Title } from '@shoutem/ui'
 
 class HomeContainer extends Component {
   constructor (props) {
@@ -10,14 +11,37 @@ class HomeContainer extends Component {
     this.state = {
       delegateName: 'doweig',
     }
+
+    this.getDelegateInfo()
+  }
+
+  getDelegateInfo = () => {
+    return fetch('https://explorer.lisk.io/api/getSearch?q=' + this.state.delegateName)
+      .then((response) => response.json())
+      .then((responseJson) => fetch('https://explorer.lisk.io/api/getAccount?address=' + responseJson.address))
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
   render () {
     return (
       <View>
-        <Text>
-          Hello {this.state.delegateName}, welcome to Ark!
-        </Text>
+        <View>
+          <NavigationBar
+            hasHistory
+            centerComponent={<Title>{this.state.delegateName}</Title>}
+          />
+        </View>
+        <View>
+          <Text>
+            Hello {this.state.delegateName}, welcome to Ark!
+          </Text>
+        </View>
       </View>
     )
   }
