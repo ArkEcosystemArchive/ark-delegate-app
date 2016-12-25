035566f61ccf7102107eb066d9220c05ce332e5b
Guillaume Verbal <guillaume.verbal@gmail.com>
2016-12-25 22:06:29 +0800

    add next block time estimation and minor cleanups

--- src/Containers/DelegateInfo/index.js
+++ src/Containers/DelegateInfo/index.js
@@ -18,6 +18,8 @@ import {
 } from '@shoutem/ui'
 import { NavigationBar } from '@shoutem/ui/navigation'
 
+import CONST from '../../Helpers/Const'
+
 class DelegateInfo extends Component {
 
   static propTypes = {
@@ -28,19 +30,25 @@ class DelegateInfo extends Component {
     super(props)
 
     this.state = {
+      firstFetchDone: false,
+
       // delegateName: props.delegateName,
       delegateName: 'doweig',
+
       delegateAddress: '',
       account: null,
-      lastBlock: null,
-      firstFetchDone: false,
-    }
 
+      lastBlock: null,
+      lastBlockTimeHuman: '',
+
+      nextForgerPosition: 0,
+      nextBlockTimeHuman: '',
+    }
   }
 
   componentDidMount () {
     this.startDelegateInfoRefresher()
-    // this.startLastBlockTimeRefresher()
+    this.startLastBlockTimeRefresher()
   }
 
   startLastBlockTimeRefresher = () => {
@@ -57,24 +65,21 @@ class DelegateInfo extends Component {
     this.setState({
       ...this.state,
       lastBlockTimeHuman: humanize.relativeTime(this.state.lastBlock.forgedTS),
+      nextBlockTimeHuman: humanize.relativeTime(
+        humanize.time() + (this.state.nextForgerPosition * CONST.BLOCKTIME_IN_SECONDS)
+      ),
     })
-  }
-
-  getCurrentTS () {
-    return Math.floor(new Date().getTime() / 1000)
   }
 
   getTSfromEpochStamp (epochStamp) {
     return Math.floor(
       new Date(
-        (((Date.UTC(2016, 4, 24, 17, 0, 0, 0) / 1000) + epochStamp) * 1000)
+        (((CONST.GENESIS_BLOCK_TS / 1000) + epochStamp) * 1000)
       ).getTime() / 1000
     )
   }
 
   startDelegateInfoRefresher = () => {
-    this.setState({ ...this.state, firstFetchDone: true })
-
     return fetch('http://10.10.11.56:6040/api/getSearch?q=' + this.state.delegateName)
       .then((response) => response.json())
       .then((responseJson) => {
@@ -85,7 +90,7 @@ class DelegateInfo extends Component {
 
         setInterval(() => {
           this.getDelegateInfo(this.state.delegateAddress)
-        }, 1000)
+        }, 5 *1000)
       })
       .catch((error) => {
         console.error(error)
@@ -112,6 +117,15 @@ class DelegateInfo extends Component {
           ...this.state,
           lastBlock: block,
         })
+      })
+      .then((responseJson) => fetch('http://10.10.11.56:6040/api/delegates/getNextForgers'))
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const { delegates } = responseJson
+        this.setState({
+          ...this.state,
+          nextForgerPosition: delegates.indexOf(this.state.account.publicKey),
+        })
         this.setLastBlockTimeHuman()
         this.setState({
           ...this.state,
@@ -124,9 +138,9 @@ class DelegateInfo extends Component {
   }
 
   render() {
-    const { fetching, delegateName, account, lastBlockTimeHuman, lastBlock } = this.state
-
-    if (fetching || !account) {
+    const state = this.state
+    
+    if (!state.firstFetchDone) {
       return (
         <Screen>
           <Text>Loading...</Text>
@@ -135,9 +149,8 @@ class DelegateInfo extends Component {
     }
 
     return (
-
       <Screen>
-        <NavigationBar title={delegateName} />
+        <NavigationBar title={state.delegateName} />
         <ScrollView>
           
           <Screen styleName="paper">
@@ -151,35 +164,42 @@ class DelegateInfo extends Component {
             <Row>
               <View>
                 <Subtitle>Last forged block</Subtitle>
-                <Text>{lastBlockTimeHuman}</Text>
+                <Text>{state.lastBlockTimeHuman}</Text>
+              </View>
+            </Row>
+            <Divider styleName="line" />
+            <Row>
+              <View>
+              <Subtitle>Next forged block (estimation)</Subtitle>
+                <Text>{state.nextBlockTimeHuman}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Forged</Subtitle>
-                <Text>{account.delegate.forged / 100000000} ARK</Text>
+                <Text>{state.account.delegate.forged / 100000000} ARK</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Productivity</Subtitle>
-                <Text>{account.delegate.productivity} %</Text>
+                <Text>{state.account.delegate.productivity} %</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Missed Blocks</Subtitle>
-                <Text>{account.delegate.missedblocks}</Text>
+                <Text>{state.account.delegate.missedblocks}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Produced Blocks</Subtitle>
-                <Text>{account.delegate.producedblocks}</Text>
+                <Text>{state.account.delegate.producedblocks}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
@@ -193,14 +213,14 @@ class DelegateInfo extends Component {
             <Row>
               <View>
                 <Subtitle>Approval</Subtitle>
-                <Text>{account.delegate.approval} %</Text>
+                <Text>{state.account.delegate.approval} %</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Position</Subtitle>
-                <Text>{account.delegate.rate} / 51</Text>
+                <Text>{state.account.delegate.rate} / {CONST.ACTIVE_DELEGATES}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
@@ -214,21 +234,21 @@ class DelegateInfo extends Component {
             <Row>
               <View>
                 <Subtitle>Delegate Name</Subtitle>
-                <Text>{account.delegate.username}</Text>
+                <Text>{state.account.delegate.username}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Address</Subtitle>
-                <Text>{account.delegate.address}</Text>
+                <Text>{state.account.delegate.address}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
             <Row>
               <View>
                 <Subtitle>Public Key</Subtitle>
-                <Text>{account.delegate.publicKey}</Text>
+                <Text>{state.account.delegate.publicKey}</Text>
               </View>
             </Row>
             <Divider styleName="line" />
